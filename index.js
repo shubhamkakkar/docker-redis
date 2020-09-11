58fe14ff9f20bc55f6261bc071d3cf7b3032e048
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-09-12 02:13:32 +0530

    docker-compose + docker-build worked

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 // We have a service labled as redis-server so specifying that as the host..
 // docker picks that up and allows the communication to that container
+
 const client = redis.createClient({
     host: 'redis-server',
     port: 6379
@@ -12,14 +13,11 @@ const client = redis.createClient({
 
 client.set('visits', 0);
 
-console.log('here')
-
 app.get("/", (req, res) => {
     client.get('visits', (err,visits) => {
         res.status(200).json({"Number of visits": visits})
         client.set('visits', parseInt(visits) + 1)
     })
 })
-
 
 app.listen( 8080, () => console.log('running at 8080'));
